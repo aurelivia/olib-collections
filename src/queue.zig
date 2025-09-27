0f8f743a1bbb3d7e9673e84b351a4252f5d73510
Olivia Taliesin <olivia@taliesin.dev>
2025-09-27 18:44:34 +0200

    :recycle: Refactor Queue to use a semi-persistent node pool

--- src/queue.zig
+++ src/queue.zig
@@ -1,101 +1,112 @@
 const std = @import("std");
 const Mem = std.mem.Allocator;
 
-pub fn UQueue(comptime T: type) type {
-    return struct {
-        const Self = @This();
+pub fn Queue(comptime T: type) type { return struct {
+    pub const Node = struct {
+        val: T = undefined,
+        next: ?*Node = null
+    };
 
-        const Node = struct {
-            val: T = undefined,
-            next: ?*Node = null
-        };
+    fn pushNode(head: *(?*Node), last: *(?*Node), node: *Node) void {
+        var prev: ?*Node = null;
+        while (true) {
+            prev = @atomicLoad(?*Node, last, .acquire);
+            if (@cmpxchgWeak(?*Node, last, prev, node, .acq_rel, .acquire) == null) {
+                if (prev) |p| @atomicStore(?*Node, &(p.next), node, .release);
+                break;
+            }
+        }
+        _ = @cmpxchgWeak(?*Node, head, null, prev, .acq_rel, .acquire);
+    }
 
+    fn popNode(head: *(?*Node), last: *(?*Node)) ?*Node {
+        var prev: ?*Node = null;
+        while (true) {
+            prev = @atomicLoad(?*Node, head, .acquire);
+            if (prev == null) break;
+            if (@cmpxchgWeak(?*Node, head, prev, prev.?.next, .acq_rel, .acquire) == null) break;
+        }
+
+        _ = @cmpxchgWeak(?*Node, last, prev, null, .acq_rel, .acquire);
+
+        return prev;
+    }
+
+    pub const Pool = struct {
+        managed: bool = false,
+        instances: usize = 0,
         head: ?*Node = null,
         last: ?*Node = null,
 
-        pub fn deinit(self: *Self, mem: Mem) void {
+        pub fn deinit(self: *@This(), mem: Mem) void {
             self.clear(mem);
+            self.managed = false;
         }
 
-        pub fn push(self: *Self, mem: Mem, val: T) !void {
+        pub fn clear(self: *@This(), mem: Mem) void {
+            var last: ?*Node = popNode(&(self.head), &(self.last));
+            while (last) |l| {
+                mem.destroy(l);
+                last = popNode(&(self.head), &(self.last));
+            }
+        }
+    };
+
+    pub var pool: Pool = .{};
+
+    head: ?*Node = null,
+    last: ?*Node = null,
+
+    pub fn deinit(self: *@This(), mem: Mem) void {
+        self.clear();
+        pool.instances -= 1;
+        if (!pool.managed and pool.instances == 0) pool.clear(mem);
+    }
+
+    pub fn init() @This() {
+        pool.instances += 1;
+        return .{};
+    }
+
+    pub fn push(self: *@This(), mem: Mem, val: T) Mem.Error!void {
+        const node: *Node = if (popNode(&(pool.head), &(pool.last))) |n| b: {
+            n.val = val;
+            break :b n;
+        } else b: {
             const n: *Node = try mem.create(Node);
             n.* = .{ .val = val };
+            break :b n;
+        };
 
-            var prev: ?*Node = null;
-            while (true) {
-                prev = @atomicLoad(?*Node, &self.last, .acquire);
-                if (@cmpxchgWeak(?*Node, &self.last, prev, n, .acq_rel, .acquire) == null) {
-                    if (prev) |p| @atomicStore(?*Node, &(p.next), n, .release);
-                    break;
-                }
-            }
+        pushNode(&(self.head), &(self.last), node);
+    }
 
-            _ = @cmpxchgWeak(?*Node, &self.head, null, prev, .acq_rel, .acquire);
-        }
+    pub fn pop(self: *@This()) ?T {
+        if (popNode(&(self.head), &(self.last))) |prev| {
+            const val = prev.val;
+            pushNode(&(pool.head), &(pool.last), prev);
+            return val;
+        } else return null;
+    }
 
-        pub fn pop(self: *Self, mem: Mem) ?T {
-            var prev: ?*Node = null;
-            while (true) {
-                prev = @atomicLoad(?*Node, &self.head, .acquire);
-                if (prev == null) break;
-                if (@cmpxchgWeak(?*Node, &self.head, prev, prev.?.next, .acq_rel, .acquire) == null) break;
-            }
-
-            _ = @cmpxchgWeak(?*Node, &self.last, prev, null, .acq_rel, .acquire);
-
-            if (prev) |p| {
-                const val = p.val;
-                mem.destroy(p);
-                return val;
-            } return null;
-        }
-
-        pub fn clear(self: *Self, mem: Mem) void {
-            var last: ?T = self.pop(mem);
-            while (last != null) last = self.pop(mem);
-        }
-    };
-}
-
-pub fn Queue(comptime T: type) type {
-    return struct {
-        const Self = @This();
-
-        inner: UQueue(T) = .{},
-        mem: Mem = undefined,
-
-        pub fn init(self: *Self, mem: Mem) void {
-            self.mem = mem;
-        }
-
-        pub fn deinit(self: *Self) void {
-            self.inner.deinit(self.mem);
-        }
-
-        pub fn push(self: *Self, val: T) !void {
-            try self.inner.push(self.mem, val);
-        }
-
-        pub fn pop(self: *Self) ?T {
-            return self.inner.pop(self.mem);
-        }
-
-        pub fn clear(self: *Self) void {
-            self.inner.clear(self.mem);
-        }
-    };
-}
+    pub fn clear(self: *@This()) void {
+        var last: ?T = self.pop();
+        while (last != null) last = self.pop();
+    }
+};}
 
 const expect = std.testing.expect;
 
 test "Queue" {
-    var queue: Queue(u8) = .{ .mem = std.testing.allocator };
+    const mem = std.testing.allocator;
+    var queue: Queue(u8) = .init();
+    defer queue.deinit(mem);
 
-    try queue.push(1);
-    try queue.push(2);
-    try queue.push(3);
-    try queue.push(4);
-    try queue.push(5);
+    try queue.push(mem, 1);
+    try queue.push(mem, 2);
+    try queue.push(mem, 3);
+    try queue.push(mem, 4);
+    try queue.push(mem, 5);
     try expect(queue.pop() == 1);
     try expect(queue.pop() == 2);
     try expect(queue.pop() == 3);
--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,5 @@
 pub const Table = @import("./table.zig").Table;
 pub const Queue = @import("./queue.zig").Queue;
 pub const Range = @import("./range.zig").Range;
+
+test { @import("std").testing.refAllDecls(@This()); }
