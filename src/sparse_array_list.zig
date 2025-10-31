a3d10b1adc624f3e43d43aa323865b88e89714ef
Olivia Taliesin <olivia@taliesin.dev>
2025-10-31 01:22:54 +0100

    :sparkles: SparseArrayList - Getter

--- src/sparse_array_list.zig
+++ src/sparse_array_list.zig
@@ -63,8 +63,9 @@ pub fn SparseArrayList(comptime T: type) type { return struct {
     };
 
     inner: std.ArrayList(Slice),
+    trailing_nulls: if (is_optional) usize else void,
 
-    pub const empty: Self = .{ .inner = .empty };
+    pub const empty: Self = .{ .inner = .empty, .trailing_nulls = if (is_optional) 0 else {} };
 
     pub fn deinit(self: *Self, mem: Allocator) void {
         for (self.inner.items) |*slice| slice.deinit(mem);
@@ -83,6 +84,53 @@ pub fn SparseArrayList(comptime T: type) type { return struct {
             .cap = @intCast(slice.len)
         });
         return self;
+    }
+
+    inline fn knownLen(self: *const Self) usize {
+        if (self.inner.items.len == 0) return 0;
+        const last = self.inner.items[self.inner.items.len - 1];
+        return last.offset + last.len;
+    }
+
+    pub inline fn len(self: *const Self) usize {
+        if (!is_optional) @compileError("len() cannot be used with sparse arrays of non-optional types (" ++ @typeName(T) ++ ") as they are unbounded.");
+        return self.knownLen() + self.trailing_nulls;
+    }
+
+    inline fn assertBounds(self: *const Self, index: usize) void {
+        if (is_optional) {
+            if (index >= self.len()) {
+                std.debug.print("Index {d} greater than maximum bound {d} of sparse array.\n", .{ index, self.len() });
+                unreachable;
+            }
+        }
+    }
+
+    fn getPositionContaining(self: *const Self, index: usize) ?usize {
+        self.assertBounds(index);
+        if (self.inner.items.len == 0) return null;
+        var range = self.inner.items;
+        var shift: usize = 0;
+        while (true) {
+            const midpos: usize = range.len / 2;
+            const mid = range[midpos];
+            if (mid.offset <= index) {
+                if (index < (mid.offset + mid.len)) return shift + midpos;
+                if ((midpos + 1) == range.len) return null;
+                range = range[(midpos + 1)..];
+                shift += midpos + 1;
+            } else {
+                if (range.len == 1) return null;
+                range = range[0..midpos];
+            }
+        }
+    }
+
+    pub fn get(self: *const Self, index: usize) LookupResult {
+        if (self.getPositionContaining(index)) |pos| {
+            const slice = self.inner.items[pos];
+            return slice.ptr[index - slice.offset];
+        } else return null;
     }
 };}
 
@@ -111,4 +159,42 @@ test "Slice Capacity" {
     try tst.expectEqual(false, try t.inner.items[0].ensureCapacity(test_mem, 130, 130));
     try tst.expectEqual(130, t.inner.items[0].len);
     try tst.expectEqual(130, t.inner.items[0].cap);
+}
+
+fn appendTestSlice(t: *TestUnbounded, off: usize, len: u32) OOM!void {
+    const slice = try test_mem.alloc(usize, len);
+    for (0..slice.len) |i| slice[i] = i + off;
+    errdefer test_mem.free(slice);
+    try t.inner.append(test_mem, .{
+        .offset = off,
+        .ptr = slice.ptr,
+        .len = len,
+        .cap = @intCast(slice.len)
+    });
+}
+
+fn makeTestList() OOM!TestUnbounded {
+    var t: TestUnbounded = .empty;
+    errdefer t.deinit(test_mem);
+
+    try appendTestSlice(&t, 0, 1);
+    try appendTestSlice(&t, 4, 2);
+    try appendTestSlice(&t, 8, 4);
+    try appendTestSlice(&t, 16, 8);
+    try appendTestSlice(&t, 32, 16);
+
+    return t;
+}
+
+test "Get" {
+    var t: TestUnbounded = try makeTestList();
+    defer t.deinit(test_mem);
+
+    try tst.expectEqual(0, t.get(0));
+    var s: usize = 4;
+    while (s <= 16): (s <<= 1) {
+        const l: usize = s / 2;
+        for (0..l) |i| try tst.expectEqual(s + i, t.get(s + i));
+        for (l..s) |i| try tst.expectEqual(null, t.get(s + i));
+    }
 }
